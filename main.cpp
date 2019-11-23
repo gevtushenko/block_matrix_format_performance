3e5336113f03076e8a367401f47a80eb402d98f6
Evtushenko Georgy <evtushenko.georgy@gmail.com>
2019-11-24 00:16:12 +0300

    Allocate csr matrix by bcsr

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
+#include "measurement_class.h"
+
 #include <iostream>
+#include <chrono>
 #include <memory>
 
 template <typename data_type, typename index_type>
@@ -28,9 +31,80 @@ public:
   const index_type nnzb {};
 
   const std::unique_ptr<data_type> values;
-  const std::unique_ptr<index_type> columns;
-  const std::unique_ptr<index_type> row_ptr;
+  const std::unique_ptr<index_type[]> columns;
+  const std::unique_ptr<index_type[]> row_ptr;
 };
+
+template <typename data_type, typename index_type>
+class csr_matrix_class
+{
+public:
+  explicit csr_matrix_class (const bcsr_matrix_class<data_type, index_type> &matrix)
+    : n_rows (matrix.n_rows)
+    , n_cols (matrix.n_cols)
+    , nnz (matrix.nnzb * matrix.bs * matrix.bs)
+    , values (new data_type[nnz])
+    , columns (new index_type[nnz])
+    , row_ptr (new index_type[n_rows + 1])
+  {
+    auto brow_ptr = matrix.row_ptr.get ();
+    for (index_type row = 0; row <= n_rows; row++)
+      row_ptr[row] = brow_ptr[row] * matrix.bs;
+  }
+
+public:
+  const index_type n_rows {};
+  const index_type n_cols {};
+
+  const index_type nnz {};
+
+  const std::unique_ptr<data_type> values;
+  const std::unique_ptr<index_type[]> columns;
+  const std::unique_ptr<index_type[]> row_ptr;
+};
+
+template<typename data_type, typename index_type>
+measurement_class cpu_csr_spmv_single_thread_naive (
+  const csr_matrix_class<data_type, index_type> &matrix,
+  data_type *x,
+  data_type *y)
+{
+  fill_n (x, matrix.meta.cols_count, 1.0);
+
+  const auto row_ptr = matrix.row_ptr.get ();
+  const auto col_ids = matrix.columns.get ();
+  const auto data = matrix.data.get ();
+
+  auto begin = std::chrono::system_clock::now ();
+
+  for (index_type row = 0; row < matrix.meta.rows_count; row++)
+    {
+      const auto row_start = row_ptr[row];
+      const auto row_end = row_ptr[row + 1];
+
+      data_type dot = 0;
+      for (auto element = row_start; element < row_end; element++)
+        dot += data[element] * x[col_ids[element]];
+      y[row] = dot;
+    }
+
+  auto end = std::chrono::system_clock::now ();
+  const double elapsed = std::chrono::duration<double> (end - begin).count ();
+
+  const size_t data_bytes = matrix.meta.non_zero_count * sizeof (data_type);
+  const size_t x_bytes = matrix.meta.non_zero_count * sizeof (data_type);
+  const size_t col_ids_bytes = matrix.meta.non_zero_count * sizeof (index_type);
+  const size_t row_ids_bytes = 2 * matrix.meta.rows_count * sizeof (index_type);
+  const size_t y_bytes = matrix.meta.rows_count * sizeof (data_type);
+
+  const size_t operations_count = matrix.meta.non_zero_count * 2; // + and * per element
+
+  return measurement_class (
+    "CPU CSR",
+    elapsed,
+    data_bytes + x_bytes + col_ids_bytes + row_ids_bytes + y_bytes,
+    operations_count);
+}
 
 template <typename data_type, typename index_type>
 std::unique_ptr<bcsr_matrix_class<data_type, index_type>> gen_n_diag_bcsr (
@@ -68,6 +142,7 @@ std::unique_ptr<bcsr_matrix_class<data_type, index_type>> gen_n_diag_bcsr (
 
 int main ()
 {
-  auto matrix = gen_n_diag_bcsr<float, int> (8, 5, 2);
+  auto block_matrix = gen_n_diag_bcsr<float, int> (8, 5, 2);
+  auto matrix = std::make_unique<csr_matrix_class<float, int>> (*block_matrix);
   return 0;
 }
---
+++ measurement_class.h
@@ -0,0 +1,92 @@
+//
+// Created by egi on 11/24/19.
+//
+
+#ifndef BLOCK_MATRIX_FORMAT_PERFORMANCE_MEASUREMENT_CLASS_H
+#define BLOCK_MATRIX_FORMAT_PERFORMANCE_MEASUREMENT_CLASS_H
+
+#include <string>
+#include <iostream>
+
+class measurement_class
+{
+  const size_t giga = 1E+9;
+
+public:
+  measurement_class () = default;
+  measurement_class (
+    std::string format,
+    double elapsed,
+    double load_store_bytes,
+    double operations_count);
+
+  double get_elapsed () const { return elapsed; }
+  double get_effective_bandwidth () const { return effective_bandwidth; }
+  double get_computational_throughput () const { return computational_throughput; }
+
+  const std::string &get_format () const { return matrix_format; }
+
+  measurement_class & operator+=(const measurement_class &rhs)
+  {
+    elapsed += rhs.get_elapsed ();
+    effective_bandwidth += rhs.get_effective_bandwidth ();
+    computational_throughput += rhs.get_computational_throughput ();
+
+    matrix_format = rhs.get_format ();
+    measurements_count++;
+
+    return *this;
+  }
+
+  void finalize ()
+  {
+    if (measurements_count)
+      {
+        elapsed /= measurements_count;
+        effective_bandwidth /= measurements_count;
+        computational_throughput /= measurements_count;
+      }
+  }
+
+private:
+  double elapsed {};
+  double effective_bandwidth {};
+  double computational_throughput {};
+  std::string matrix_format;
+
+  unsigned int measurements_count {};
+};
+
+template <typename data_type>
+void compare_results (unsigned int y_size, const data_type *a, const data_type *b)
+{
+  data_type numerator = 0.0;
+  data_type denumerator = 0.0;
+
+  for (unsigned int i = 0; i < y_size; i++)
+    {
+      numerator += (a[i] - b[i]) * (a[i] - b[i]);
+      denumerator += b[i] * b[i];
+    }
+
+  const data_type error = numerator / denumerator;
+
+  if (error > 1e-9)
+    {
+      std::cerr << "ERROR: " << error << std::endl;
+
+      for (unsigned int i = 0; i < y_size; i++)
+        {
+          if (std::abs (a[i] - b[i]) > 1e-8)
+            {
+              std::cerr << "a[" << i << "] = " << a[i] << "; b[" << i << "] = " << b[i] << std::endl;
+              break;
+            }
+        }
+    }
+
+  std::cerr.flush ();
+}
+
+
+#endif //BLOCK_MATRIX_FORMAT_PERFORMANCE_MEASUREMENT_CLASS_H
