9ff7915a2c78e41316c1f7a5187c38780586c74b
Evtushenko Georgy <evtushenko.georgy@gmail.com>
2019-11-24 19:33:57 +0300

    Add cuSPARSE BSR

--- gpu/gpu_matrix_multiplier.cu
+++ gpu/gpu_matrix_multiplier.cu
@@ -1,4 +1,5 @@
 #include <cuda_runtime.h>
+#include <cusparse_v2.h>
 
 #include "gpu_matrix_multiplier.h"
 
@@ -228,6 +229,66 @@ __global__ void bcsr_spmv_kernel_block_per_block_row_thread_per_row_column_major
   y[block_row * bs + row] = local_out;
 }
 
+void cusparse_bsrmv (
+  cusparseHandle_t  &handle,
+  cusparseMatDescr_t  &descr_A,
+  cusparseDirection_t direction,
+
+  int n_rows,
+  int n_cols,
+  int nnzb,
+  int bs,
+
+  const float *A,
+  const int *row_ptr,
+  const int *columns,
+  const float *x,
+  float *y
+  )
+{
+  const float alpha = 1.0;
+  const float beta = 0.0;
+
+  cusparseSbsrmv (
+    handle,
+    direction,
+    CUSPARSE_OPERATION_NON_TRANSPOSE,
+    n_rows, n_cols, nnzb,
+    &alpha, descr_A, A,
+    row_ptr, columns, bs,
+    x, &beta, y);
+}
+
+void cusparse_bsrmv (
+  cusparseHandle_t  &handle,
+  cusparseMatDescr_t  &descr_A,
+  cusparseDirection_t direction,
+
+  int n_rows,
+  int n_cols,
+  int nnzb,
+  int bs,
+
+  const double *A,
+  const int *row_ptr,
+  const int *columns,
+  const double *x,
+  double *y
+)
+{
+  const double alpha = 1.0;
+  const double beta = 0.0;
+
+  cusparseDbsrmv (
+    handle,
+    direction,
+    CUSPARSE_OPERATION_NON_TRANSPOSE,
+    n_rows, n_cols, nnzb,
+    &alpha, descr_A, A,
+    row_ptr, columns, bs,
+    x, &beta, y);
+}
+
 template <typename data_type, typename index_type>
 std::vector<measurement_class> gpu_bcsr_spmv (
   bcsr_matrix_class<data_type, index_type> &matrix,
@@ -303,6 +364,44 @@ std::vector<measurement_class> gpu_bcsr_spmv (
 
   compare_results (y_size, reference_y, cpu_y.get ());
 
+  /// cuSPARSE Row major
+  {
+    cusparseHandle_t handle;
+    cusparseCreate (&handle);
+
+    cusparseMatDescr_t descr_A;
+    cusparseCreateMatDescr (&descr_A);
+    cusparseSetMatType (descr_A, CUSPARSE_MATRIX_TYPE_GENERAL);
+    cusparseSetMatIndexBase (descr_A, CUSPARSE_INDEX_BASE_ZERO);
+
+    cudaEvent_t start, stop;
+    cudaEventCreate (&start);
+    cudaEventCreate (&stop);
+
+    cudaDeviceSynchronize ();
+    cudaEventRecord (start);
+
+    cusparse_bsrmv (handle, descr_A, CUSPARSE_DIRECTION_ROW, matrix.n_rows, matrix.n_cols, matrix.nnzb, matrix.bs, d_values, d_row_ptr, d_columns, d_x, d_y);
+
+    cudaEventRecord (stop);
+    cudaEventSynchronize (stop);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime (&milliseconds, start, stop);
+    const double elapsed = milliseconds / 1000;
+
+    cudaEventDestroy (start);
+    cudaEventDestroy (stop);
+
+    cusparseDestroyMatDescr (descr_A);
+    cusparseDestroy (handle);
+
+    results.emplace_back ("GPU BSR (cuSPARSE, row major)", elapsed, 0, 0);
+
+    cudaMemcpy (cpu_y.get (), d_y, y_size * sizeof (data_type), cudaMemcpyDeviceToHost);
+    compare_results (y_size, reference_y, cpu_y.get ());
+  }
+
   {
     dim3 block_size = dim3 (512);
     dim3 grid_size {};
@@ -313,6 +412,44 @@ std::vector<measurement_class> gpu_bcsr_spmv (
 
   matrix.transpose_blocks ();
   cudaMemcpy (d_values, matrix.values.get (), matrix_size * sizeof (data_type), cudaMemcpyHostToDevice);
+
+  /// cuSPARSE Column major
+  {
+    cusparseHandle_t handle;
+    cusparseCreate (&handle);
+
+    cusparseMatDescr_t descr_A;
+    cusparseCreateMatDescr (&descr_A);
+    cusparseSetMatType (descr_A, CUSPARSE_MATRIX_TYPE_GENERAL);
+    cusparseSetMatIndexBase (descr_A, CUSPARSE_INDEX_BASE_ZERO);
+
+    cudaEvent_t start, stop;
+    cudaEventCreate (&start);
+    cudaEventCreate (&stop);
+
+    cudaDeviceSynchronize ();
+    cudaEventRecord (start);
+
+    cusparse_bsrmv (handle, descr_A, CUSPARSE_DIRECTION_COLUMN, matrix.n_rows, matrix.n_cols, matrix.nnzb, matrix.bs, d_values, d_row_ptr, d_columns, d_x, d_y);
+
+    cudaEventRecord (stop);
+    cudaEventSynchronize (stop);
+
+    float milliseconds = 0;
+    cudaEventElapsedTime (&milliseconds, start, stop);
+    const double elapsed = milliseconds / 1000;
+
+    cudaEventDestroy (start);
+    cudaEventDestroy (stop);
+
+    cusparseDestroyMatDescr (descr_A);
+    cusparseDestroy (handle);
+
+    results.emplace_back ("GPU BSR (cuSPARSE, column major)", elapsed, 0, 0);
+
+    cudaMemcpy (cpu_y.get (), d_y, y_size * sizeof (data_type), cudaMemcpyDeviceToHost);
+    compare_results (y_size, reference_y, cpu_y.get ());
+  }
 
   {
     cudaEvent_t start, stop;
