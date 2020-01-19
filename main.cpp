d63b9a8779e1d5762ffbd9f4e2d3c8da09fbf043
Evtushenko Georgy <evtushenko.georgy@gmail.com>
2020-01-20 00:59:43 +0300

    Add jit example

--- main.cpp
+++ main.cpp
@@ -112,6 +112,22 @@ public:
   }
 };
 
+#include "cuda_jit.h"
+
+template <typename index_type>
+index_type round_up_to_power_of_two (index_type v)
+{
+  v--;
+  v |= v >> 1;
+  v |= v >> 2;
+  v |= v >> 4;
+  v |= v >> 8;
+  v |= v >> 16;
+  v++;
+
+  return v;
+}
+
 template<typename data_type, typename index_type>
 std::unordered_map<std::string, double> perform_measurements (
   csr_matrix_class<data_type, index_type> &matrix,
@@ -119,7 +135,7 @@ std::unordered_map<std::string, double> perform_measurements (
 )
 {
   std::unordered_map<std::string, double> results;
-  const unsigned int measurements_count = 40;
+  const unsigned int measurements_count = 10;
   auto measure_multiple_times = [&] (const std::function<measurement_class (bool)> &action)
   {
     measurement_class result;
@@ -150,8 +166,132 @@ std::unordered_map<std::string, double> perform_measurements (
   auto gpu_elapsed_csr_vector = measure_multiple_times ([&] (bool) { return gpu_csr_vector_spmv<data_type, index_type> (matrix, reference_answer.get ()); });
   single_core_timer.print_time (gpu_elapsed_csr_vector);
 
+
   std::unique_ptr<data_type[]> transposed_matrix_data (new data_type[block_matrix.size ()]);
   block_matrix.transpose_blocks (transposed_matrix_data.get ());
+
+  dim3 block_size = 32;
+  dim3 grid_size {};
+
+  grid_size.x = (block_matrix.n_rows * 32 + block_size.x - 1) / block_size.x;
+
+  jit(bcsr_jit,
+  {
+    const int bs = {{ bs }};
+
+    const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int lane = idx % 32;
+    const int block_row = idx / 32; ///< Warp per block row
+    const int first_block = row_ptr[block_row];
+    const int last_block = row_ptr[block_row + 1];
+
+    int col = first_block * bs + lane / bs;
+    int r = lane % bs;
+
+    __shared__ float partial_sums[{{ shared_size }}]; // = shared_memory<float> (); ///< Size is equal to blockDim.x * sizeof(float)
+
+    float local_out = 0.0;
+
+    for (; col < last_block * bs; col += 32 / bs)
+      {
+        const int block = col / bs;
+        const int c = col % bs;
+
+        const float value = data[block * bs * bs + c * bs + r];
+        const float x_value = x[col_ids[block] * bs + c];
+        local_out += x_value * value;
+      }
+
+    partial_sums[threadIdx.x] = local_out;
+
+    for (int stride = {{ stride_begin }} ; stride > 0; stride /= 2)
+      {
+        __syncthreads ();
+        if ((lane < stride * bs) && ((threadIdx.x + stride * bs) < 32))
+          {
+            partial_sums[threadIdx.x] += partial_sums[threadIdx.x + stride * bs];
+          }
+      }
+
+    if (lane < bs)
+      {
+        y[block_row * bs + lane] = partial_sums[threadIdx.x];
+      }
+  },
+    (const int *, col_ids),
+    (const int *, row_ptr),
+    (const float *, data),
+    (const float *, x),
+    (float*, y));
+  nlohmann::json json;
+  json["bs"] = bs;
+  json["stride_begin"] = round_up_to_power_of_two((32 / bs) / 2);
+  json["shared_size"] = block_size.x;
+  auto bcsr_kernel = bcsr_jit.compile (json);
+
+  const index_type matrix_size = block_matrix.nnzb * block_matrix.bs * block_matrix.bs;
+  const index_type columns_size = block_matrix.nnzb;
+  const index_type row_ptr_size = block_matrix.n_rows + 1;
+  const index_type x_size = block_matrix.n_cols * block_matrix.bs;
+  const index_type y_size = block_matrix.n_rows * block_matrix.bs;
+
+  data_type *d_values {};
+  data_type *d_y {};
+  data_type *d_x {};
+
+  index_type *d_row_ptr {};
+  index_type *d_columns {};
+
+  cudaMalloc (&d_values, matrix_size * sizeof (data_type));
+  cudaMalloc (&d_x, x_size * sizeof (data_type));
+  cudaMalloc (&d_y, y_size * sizeof (data_type));
+
+  cudaMalloc (&d_row_ptr, row_ptr_size * sizeof (index_type));
+  cudaMalloc (&d_columns, columns_size * sizeof (index_type));
+
+  cudaMemcpy (d_values, transposed_matrix_data.get (), matrix_size * sizeof (data_type), cudaMemcpyHostToDevice);
+  cudaMemcpy (d_columns, block_matrix.columns.get (), columns_size * sizeof (index_type), cudaMemcpyHostToDevice);
+  cudaMemcpy (d_row_ptr, block_matrix.row_ptr.get (), row_ptr_size * sizeof (index_type), cudaMemcpyHostToDevice);
+
+  std::unique_ptr<float[]> h_x (new float[x_size]);
+  std::fill_n (h_x.get (), x_size, 1.0);
+
+  cudaMemcpy (d_x, h_x.get (), x_size * sizeof (float), cudaMemcpyHostToDevice);
+
+  cudaEvent_t start, stop;
+  cudaEventCreate (&start);
+  cudaEventCreate (&stop);
+
+  cudaDeviceSynchronize ();
+  cudaEventRecord (start);
+
+  bcsr_kernel.launch (grid_size, block_size, d_columns, d_row_ptr, d_values, d_x, d_y);
+
+  cudaEventRecord (stop);
+  cudaEventSynchronize (stop);
+
+  std::unique_ptr<data_type[]> cpu_y (new data_type[y_size]);
+  cudaMemcpy (cpu_y.get (), d_y, y_size * sizeof (data_type), cudaMemcpyDeviceToHost);
+
+  compare_results (y_size, reference_answer.get (), cpu_y.get ());
+
+  cudaFree (d_values);
+  cudaFree (d_x);
+  cudaFree (d_y);
+  cudaFree (d_row_ptr);
+  cudaFree (d_columns);
+
+  float milliseconds = 0;
+  cudaEventElapsedTime (&milliseconds, start, stop);
+  const double elapsed = milliseconds / 1000;
+
+  cudaEventDestroy (start);
+  cudaEventDestroy (stop);
+
+  results["jit"] = elapsed;
+
+  measurement_class jit_measure ("jit", elapsed, 0.0, 0.0);
+  single_core_timer.print_time (jit_measure);
 
   std::vector<measurement_class> multiple_measurements = gpu_bcsr_spmv<data_type, index_type> (block_matrix, transposed_matrix_data.get (), reference_answer.get ());
 
@@ -257,8 +397,9 @@ int main ()
   std::ofstream os ("result.json");
   os << json.dump (2) << std::endl;
 
-  if (0)
+  #if 0
     measure_golden_bridge<double, int> (false);
+  #endif
 
   return 0;
 }
